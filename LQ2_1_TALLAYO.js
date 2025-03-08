5eb4dc2ca8682027f4d34ba579f6e1f2c7205daf
cinnamorollsanrio <princesspadaloy45@gmail.com>
2025-03-08 12:20:48 +0800

    Moved file to main repository

---
+++ LQ2_1_TALLAYO.js
@@ -0,0 +1,8 @@
+let user_name = prompt("Enter your username:");
+let user_password = prompt("Enter your password:");
+
+if (user_name === "cinnamorollsanrio" && user_password === "iloveusababyko") {
+    alert('Welcome ${user_name}');
+ }  else {
+    alert('Maybe Username or Password is Invalid or Does not Match')
+ }
---
+++ LQ2_2_TALLAYO.js
@@ -0,0 +1,6 @@
+let dog_breed = prompt("Enter dog breed:");
+let print_count = parseInt(prompt("Enter print count:"), 5);
+
+for (let i = 0; i < print_count; i++) {
+    console.log(dog_breed);
+}
